72ae5e134f3e81debca2f1ba3bc9310a7649cece
omirostr1 <115593155+omirostr1@users.noreply.github.com>
2024-07-15 18:33:47 +0100

    finished exercise 5 and 6

--- src/main/scala/FlowExercises.scala
+++ src/main/scala/FlowExercises.scala
@@ -47,9 +47,48 @@ object FlowExercises extends App {
   println(factorial(3))
 
   // Exercise 5.
-//
-//  def fibonacci(num: Int): Int = {
-//
+
+  def fibonacci(num: Int): Int = {
+    if (num == 0) 0
+    else if (num == 1 || num == 2) 1
+    else fibonacci(num - 1) + fibonacci(num - 2)
+  }
+
+  println(fibonacci(0))
+  println(fibonacci(1))
+  println(fibonacci(2))
+  println(fibonacci(7))
+  println(fibonacci(11))
+  println(fibonacci(14))
+
+  // Exercise 6.
+
+  def multiplicationTable(num: Int): Unit = {
+    def helper(reminder: Int): Unit = {
+      if (reminder > 0) {
+        println(s"$num * $reminder = " + num * reminder)
+        helper(reminder - 1)
+      } else {
+        println(s"$num * $reminder = " + num * 0)
+      }
+    }
+    println(s"The multiplication table of $num is:")
+    helper(num)
+  }
+
+  multiplicationTable(3)
+
+  // Exercise 7.
+
+//  def summation(array: Array): Int = {
+//    def helper(position: Int): Int = {
+//      if (position < array.length) array(position) + array(helper(position + 1))
+//      else
+//    }
+//    helper(0)
 //  }
+//
+//  println(summation(Array[1, 2, 3, 4, 5, 6]))
+//  println(summation(Array[0, 2, 43, 4, -5, 16]))
 
 }
